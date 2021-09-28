9918e7b97c66337bc77dd4e500c4eab8cc89cf5b
ls <cancel>
2021-09-28 08:08:44 -0500

    fetching APIs

--- nftparent/src/Components/Bottle.jsx
+++ nftparent/src/Components/Bottle.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Bottle(props) {
+
+    // http://localhost:8000/bottle/?format=json
+    const urlBottle = 'http://localhost:8000/bottle/?format=json'
+    const [bottle, setBottle ] = useState([])
+
+
+        function getUrl() {
+            fetch(urlBottle)
+            .then(res => res.json())
+            .then(res => {
+                console.log('Success fetch', res)
+                setBottle(res)
+                console.log('this is bottle in useState', bottle)
+                
+            })
+            .catch(console.error())
+        }
+
+    useEffect(() => {
+        getUrl()
+    },[])
+
     return (
         <div>
             
